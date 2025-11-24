e549f2eaa46fbd7b3e7b36f9651627b40f48029b
Севастьянов <idatah55.gmail.com>
2025-11-24 12:37:49 +0300

    delete тест.js

--- src/тест.js
+++
@@ -1,9 +0,0 @@
-const test = (operation, check) => {
-let quest = readlineSync.question(`Question: ${operation} `);
-if (quest === operation) {
-    console.log(correct)
-} else {
-console.log('mistake')
-}
-
-}
