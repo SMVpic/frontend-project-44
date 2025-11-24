cdc9b42842cbcc6c0b8fede79400af2ae3c28ba9
Севастьянов <idatah55.gmail.com>
2025-11-24 14:13:18 +0300

    add "." in description of game brain-gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync'
 import {userName} from './cli.js'
 import {logic} from '../index.js'
-console.log('Find the greatest common divisior of given numbers');
+console.log('Find the greatest common divisor of given numbers.');
 export default () => {
 let t = true;
 for (let i = 0; i < 3 && t === true; i++) {
